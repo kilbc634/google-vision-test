d9d58f9c1b6c98ca459637ba2a4bfb9e26c7d0c3
kilbc634 <tuyn76801@gmail.com>
2025-07-12 15:17:29 +0800

    init local data

---
+++ test.py
@@ -0,0 +1,33 @@
+from google.cloud import vision
+from google.oauth2 import service_account
+import io
+
+# 替換成你的金鑰路徑
+key_path = "api_key/game-server-387707-5af02280f56c.json"
+
+# 載入憑證
+credentials = service_account.Credentials.from_service_account_file(key_path)
+
+# 建立 Vision API 客戶端
+client = vision.ImageAnnotatorClient(credentials=credentials)
+
+# 使用範例：讀圖進行 OCR
+def extract_text(image_path):
+    with open(image_path, "rb") as image_file:
+        content = image_file.read()
+
+    print('readed image')
+    image = vision.Image(content=content)
+    print('set for image')
+    response = client.text_detection(image=image)
+    print('done for image')
+
+    texts = response.text_annotations
+    if not texts:
+        return ""
+
+    return texts[0].description.strip()
+
+# 範例呼叫
+text = extract_text("image/4a5s6d.png")
+print(text)
